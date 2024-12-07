cc7d54dc86f77dd9dcf692b6ea11271c644ec828
MohabEffat <99203754+MohabEffat@users.noreply.github.com>
2024-12-08 00:36:00 +0200

    Added JWT

---
+++ BubberDinner.Infrastructure/Authentication/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using BubberDinner.Application.Common.Interfaces.Authentication;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BubberDinner.Infrastructure.Authentication
+{
+    public class TokenService : ITokenService
+    {
+        private readonly JwtOptions _options;
+        public TokenService(IOptions<JwtOptions> options)
+        {
+            _options = options.Value;
+        }
+        public string GenerateToken(Guid id, string firstName, string lastName)
+        {
+
+            var creds = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.Key)), SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, $"{firstName} + {lastName}"),
+                new Claim(JwtRegisteredClaimNames.GivenName,firstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName,lastName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var handler = new JwtSecurityTokenHandler();
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                SigningCredentials = creds,
+                Subject = new ClaimsIdentity(claims),
+                Issuer = _options.Issuer,
+                Expires = DateTime.UtcNow.AddMinutes(_options.ExpiryInMins),
+                Audience = _options.Audience,
+            };
+            var token = handler.CreateToken(tokenDescriptor);
+
+            return handler.WriteToken(token);
+        }
+    }
+}
